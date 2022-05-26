49f2c7d9f58a8fcd9fe3394b0b801243554d1d5c
kirill-s576 <mr.mcdi.576@gmail.com>
2022-05-27 01:22:54 +0400

    CI fixed

--- app/main.py
+++ app/main.py
@@ -2,15 +2,31 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
+from src.router import example
 
 app = FastAPI()
 
+# Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Static, media and templates
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+# Include routers.
+app.include_router(example.router)
+
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
