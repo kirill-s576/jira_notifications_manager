4f614a057ff724110a7b19a9b1ef01ffe4063ded
kirill-s576 <mr.mcdi.576@gmail.com>
2022-06-03 15:18:54 +0400

    Tg App Fix

--- react_static/src/pages/TgWebAppMainPage.jsx
+++ react_static/src/pages/TgWebAppMainPage.jsx
@@ -3,22 +3,21 @@ function TgWebAppMainPage({}){
     const webApp = window.Telegram.WebApp
 
     React.useEffect(() => {
-        alert("Send react ready")
-        webApp.ready()
-        alert(webApp.initDataUnsafe.toString())
+        window.Telegram.WebApp.ready()
+        window.Telegram.WebApp.MainButton.enable()
+        window.Telegram.WebApp.MainButton.setText("Main Button")
+        window.Telegram.WebApp.MainButton.show()
     }, [])
 
     const clickButton = (e) => {
-        console.log(e)
         let r = webApp.sendData("Test data")
-        console.log(r)
-        alert(r)
+        alert(window.Telegram.WebApp.initDataUnsafe.user)
     }
 
     return (
         <div>
             <h1>WebApp MainPage</h1>
-            <div>{webApp.initDataUnsafe.toString()}</div>
+            <div>{window.Telegram.WebApp.initDataUnsafe.initDataUnsafe.toString()}</div>
             <button type="button"
                     onClick={clickButton}
                     className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
