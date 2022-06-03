c82556020ef57b9a6bcf5beeca8e4a81ceef5a9b
kirill-s576 <mr.mcdi.576@gmail.com>
2022-06-03 16:04:47 +0400

    Docker volumes fix

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -2,4 +2,5 @@
 
 cd /code
 
+rm -rf /code/static/react_components
 uvicorn main:app --host 0.0.0.0 --port 8000 --reload --ws-ping-interval 120 --ws-ping-timeout 120
--- react_static/src/pages/TgWebAppMainPage.jsx
+++ react_static/src/pages/TgWebAppMainPage.jsx
@@ -10,14 +10,13 @@ function TgWebAppMainPage({}){
     }, [])
 
     const clickButton = (e) => {
-        let r = webApp.sendData("Test data")
         alert(window.Telegram.WebApp.initDataUnsafe.user)
+        let r = webApp.sendData("Test data")
     }
 
     return (
         <div>
             <h1>WebApp MainPage</h1>
-            <div>{window.Telegram.WebApp.initDataUnsafe.initDataUnsafe.toString()}</div>
             <button type="button"
                     onClick={clickButton}
                     className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
