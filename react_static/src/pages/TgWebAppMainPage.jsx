7233912d899bc16ca324f2c312e2183fc0197095
kirill-s576 <mr.mcdi.576@gmail.com>
2022-06-03 14:50:12 +0400

    Tg app debug

--- react_static/src/pages/TgWebAppMainPage.jsx
+++ react_static/src/pages/TgWebAppMainPage.jsx
@@ -5,6 +5,7 @@ function TgWebAppMainPage({}){
     React.useEffect(() => {
         alert("Send react ready")
         webApp.ready()
+        alert(webApp.initDataUnsafe.toString())
     }, [])
 
     const clickButton = (e) => {
@@ -17,6 +18,7 @@ function TgWebAppMainPage({}){
     return (
         <div>
             <h1>WebApp MainPage</h1>
+            <div>{webApp.initDataUnsafe.toString()}</div>
             <button type="button"
                     onClick={clickButton}
                     className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
